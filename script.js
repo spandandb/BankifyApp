a7c0b807e804ebe6a90f91a8c052217e52ed15e3
spandandb <spandandb94@gmail.com>
2025-02-07 21:49:19 +0530

    Date showing of transaction feature added

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
     '2020-07-11T23:36:17.929Z',
     '2020-07-12T10:51:36.790Z',
+    '2024-11-18T21:31:17.178Z',
+    '2025-01-28T09:15:04.904Z',
+    '2025-02-04T10:17:24.185Z',
+    '2025-02-01T07:42:02.383Z',
+    '2025-02-06T14:11:59.604Z',
   ],
   currency: 'INR',
   locale: 'en-IN',
@@ -48,7 +48,7 @@ const account3 = {
   interestRate: 0.7,
   pin: 3333,
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
+    '2025-02-01T13:15:33.035Z',
     '2019-11-30T09:48:16.867Z',
     '2019-12-25T06:04:23.907Z',
     '2020-01-25T14:18:46.235Z',
@@ -107,6 +107,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 // ? Functions
 
+const dateFormatter = function (date) {
+  const dateDiff = Math.trunc((new Date() - date) / (1000 * 60 * 60 * 24));
+
+  if (dateDiff === 0) return 'Today';
+  if (dateDiff === 1) return 'Yesterday';
+  if (dateDiff <= 7) return `${dateDiff} days ago`;
+
+  return `${date.getDate().toString().padStart(2, '0')}/${String(
+    date.getMonth() + 1
+  ).padStart(2, '0')}/${date.getFullYear()}`;
+};
+
 const displayMovements = function (account, sort = false) {
   const combineMovDates = account.movements.map((mov, i) => ({
     movement: mov,
@@ -123,9 +135,7 @@ const displayMovements = function (account, sort = false) {
     const typeOfMovement = mov.movement > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(mov.date);
-    const movDate = `${date.getDate().toString().padStart(2, '0')}/${String(
-      date.getMonth() + 1
-    ).padStart(2, '0')}/${date.getFullYear()}`;
+    const movDate = dateFormatter(date);
 
     containerMovements.insertAdjacentHTML(
       'afterbegin',
@@ -189,6 +199,8 @@ const refreshUI = function (account) {
 };
 
 // ? Logic
+
+containerApp.style.opacity = 0;
 
 createUsernames(accounts);
 accounts.forEach(account => {
@@ -289,8 +301,8 @@ btnSort.addEventListener('click', function (event) {
   displayMovements(currentAccount, sorted);
 });
 
-// ! FAKE LOGIN IMPLEMENTATION
+//  FAKE LOGIN IMPLEMENTATION
 
-currentAccount = accounts[0];
-refreshUI(accounts[0]);
-containerApp.style.opacity = 1;
+// currentAccount = accounts[0];
+// refreshUI(accounts[0]);
+// containerApp.style.opacity = 1;
